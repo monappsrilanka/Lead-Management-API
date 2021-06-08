5ec07484432aa0e749dbbfa69ea3be24d08c7eea
sadilchamishka <sandilchamishka@gmail.com>
2021-06-08 23:26:39 +0530

    change package model

--- models/package.js
+++ models/package.js
@@ -3,7 +3,7 @@ const schema  = mongoose.Schema;
 
 const packageSchema  = new schema({
     name:{type:String,required:true},
-    amount:{type:String,required:true},
+    amount:{type:Number,required:true},
     leads:{type:Number}
 });
 
