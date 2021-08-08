627ccec49e2a99f684722f26cd93616656116fd3
monappsrilanka <sadil.chamishka@syscolabs.com>
2021-08-09 02:02:00 +0530

    add endpoint for password reset

--- routes/agent.js
+++ routes/agent.js
@@ -8,7 +8,10 @@ const requirement = require('../models/requirement');
 const {authAgent,generateJWT} = require('../authenticate');
 const {checkHash,assignLeads} = require('../utils');
 const bcrypt = require('bcryptjs');
-const nodemailer = require("nodemailer");
+
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 var randomstring = require("randomstring");
 var mongoose = require('mongoose');
 
@@ -222,13 +225,19 @@ router.patch("/password-reset",(req,res)=>{
             if (err) throw err;
             agent.findByIdAndUpdate({_id: id},{password:password},{useFindAndModify: false},(err, agent)=> {
                 if (agent){
-                    transporter.sendMail({
-                        from: 'monapp.lk@gmail.com',
-                        to: "sadilchamishka.16@cse.mrt.ac.lk",
-                        subject: 'Password Reset for MON APP',
-                        text: "Hello world?", // plain text body
-                        html: "<b>Hello world?</b>", // html body
-                    });
+                    const msg = {
+                        to: 'sadilchamishka.16@cse.mrt.ac.lk',
+                        from: 'sadilchamishka.16@cse.mrt.ac.lk',
+                        subject: 'Sending with SendGrid is Fun',
+                        text: 'and easy to do anywhere, even with Node.js',
+                        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+                      }
+                      sgMail.send(msg).then(() => {
+                          console.log('Email sent')
+                        })
+                        .catch((error) => {
+                          console.error(error)
+                        });
                     res.json({state:true,msg:"Password Reset Successfully"});
                 }
             });   
